ffa96ad26b21c60161a31c48d1171af33f818113
clouptiburgerz <maxdenniel@gmail.com>
2022-08-27 14:15:46 +0200

    added highlight

--- lua/maxime/init.lua
+++ lua/maxime/init.lua
@@ -1,5 +1,14 @@
-require('maxime.packer')
-require('maxime.keymap')
-require('maxime.colors')
---require('maxime.lsp')
+require("maxime.packer")
+require("maxime.keymap")
+require("maxime.colors")
+--require("maxime.lsp")
 vim.cmd "set mouse=a"
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank { higroup = "IncSearch", timeout = 50 }
+	end,
+})
