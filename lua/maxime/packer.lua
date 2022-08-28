251e34397e17bc3349a5cfb2373c7e961b10082c
cloutpiburgerz <maxdenniel@gmail.com>
2022-08-28 18:38:30 +0200

    added autopair

--- lua/maxime/packer.lua
+++ lua/maxime/packer.lua
@@ -4,32 +4,36 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use 'kyazdani42/nvim-web-devicons'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use { "ellisonleao/gruvbox.nvim" }
-	use 'nvim-treesitter/nvim-treesitter'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { "ellisonleao/gruvbox.nvim" }
+    use 'nvim-treesitter/nvim-treesitter'
     use 'doums/darcula'
-	--use 'feline-nvim/feline.nvim'
-	-- 	use 'vim-airline/vim-airline'
-	-- 	use 'vim-airline/vim-airline-themes'
-	use 'KarimElghamry/vim-auto-comment'
-	-- LSP 
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
-	--use 'kabouzeid/nvim-lspinstall'
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+    --use 'feline-nvim/feline.nvim'
+    -- 	use 'vim-airline/vim-airline'
+    -- 	use 'vim-airline/vim-airline-themes'
+    use 'KarimElghamry/vim-auto-comment'
+    -- LSP 
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+    --use 'kabouzeid/nvim-lspinstall'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
     use 'jose-elias-alvarez/null-ls.nvim'
     use 'L3MON4D3/LuaSnip'
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,6 +104,12 @@ _G.packer_plugins = {
     path = "C:\\Users\\MAXIME\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
+  ["nvim-autopairs"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
+    loaded = true,
+    path = "C:\\Users\\MAXIME\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "C:\\Users\\MAXIME\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
@@ -142,6 +148,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 if should_profile then save_profiles() end
 
 end)
