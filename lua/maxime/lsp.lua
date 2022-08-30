c48326bf7429fcaa01ac074aeae3361454813584
cloutpiburgerz <maxdenniel@gmail.com>
2022-08-30 19:15:45 +0200

    changed python lsp

--- lua/maxime/lsp.lua
+++ lua/maxime/lsp.lua
@@ -40,7 +40,7 @@ local lsp_flags = {
     -- This is the default in Nvim 0.7+
     debounce_text_changes = 150,
 }
-require("lspconfig")["pylsp"].setup({
+require("lspconfig")["pyright"].setup({
     capabilities = capabilities,
     on_attach = on_attach,
     flags = lsp_flags,
