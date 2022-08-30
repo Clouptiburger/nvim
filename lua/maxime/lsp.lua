5f94fc890df9f05e872090b5b2bd29579cc0b59b
cloutpiburgerz <maxdenniel@gmail.com>
2022-08-30 19:59:48 +0200

    aa

--- lua/maxime/lsp.lua
+++ lua/maxime/lsp.lua
@@ -31,7 +31,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
     vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+    vim.keymap.set("n", "<leader>fr", vim.lsp.buf.formatting, bufopts)
     vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next)
     vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev)
 end
