5db3d2aa94adebdb0e2687a11a56a0288886564d
Clouptiburger <maxdenniel@gmail.com>
2022-08-31 18:25:53 +0200

    added fold keymap (could be better)

--- lua/maxime/keymap.lua
+++ lua/maxime/keymap.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<C-J>', '<C-W><C-J>')
 vim.keymap.set('n', '<C-H>', '<C-W><C-H>')
 vim.keymap.set('n', '<C-K>', '<C-W><C-K>')
 vim.keymap.set('n', '<C-L>', '<C-W><C-L>')
-
+vim.keymap.set('n', '<leader>--',function() vim.cmd 'set foldmethod=indent' end)
 
 -- Quick fix list short cut
 vim.keymap.set('n', "<leader>k", "<cmd>cnext<CR>zz")
