fa013f599d0f3ae972dde65b3512da2cc7d9b4df
Clouptiburger <maxdenniel@gmail.com>
2022-09-02 10:36:50 +0200

    added automatic opening on dapui

--- test/pytest.py
+++ test/pytest.py
@@ -3,8 +3,12 @@ import numpy
 
 numpy.amax
 
-def function(test : str):
+
+def function(test: str):
     return test
 
-test = function(1)
 
+test = function(1)
+if __name__ == "__main__":
+    a = 2 + 1
+    myStr = function("coucou")
