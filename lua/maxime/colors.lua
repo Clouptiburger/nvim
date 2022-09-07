4adca71ebde72cd73b92938c1716af327a4cc36a
Clouptiburger <maxdenniel@gmail.com>
2022-09-07 18:01:53 +0200

    changed colors

--- lua/maxime/colors.lua
+++ lua/maxime/colors.lua
@@ -1,4 +1,4 @@
 -- vim.cmd "colorscheme darcula"
 require('lualine').setup{options = { theme = 'dracula' }}
--- require('colorbuddy').colorscheme('gruvbuddy')
-vim.cmd "colorscheme darcula" 
+require('colorbuddy').colorscheme('gruvbuddy')
+-- vim.cmd "colorscheme darcula" 
