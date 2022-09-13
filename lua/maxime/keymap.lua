b0a2277a2de251b1f17ecb74d51587a766ebf394
Clouptiburger <maxdenniel@gmail.com>
2022-09-13 11:35:58 +0200

    added harpoon

--- lua/maxime/keymap.lua
+++ lua/maxime/keymap.lua
@@ -50,3 +50,16 @@ vim.keymap.set('n', '<leader>E', ":NvimTreeToggle<CR>")
 
 -- Markdown
 vim.keymap.set('n', '<leader>md', ":MarkdownPreviewToggle<CR>")
+
+
+vim.keymap.set('n', "<M-h><M-m>", require("harpoon.mark").add_file)
+vim.keymap.set('n', "<M-h><M-l>", require("harpoon.ui").toggle_quick_menu)
+
+for i = 1, 5 do
+    vim.keymap.set('n',
+        string.format("<space>%s", i),
+        function()
+            require("harpoon.ui").nav_file(i)
+        end
+    )
+end
