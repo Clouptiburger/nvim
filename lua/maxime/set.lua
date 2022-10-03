6fbbf2468299977ce8b87f5178883529192ebc16
cloutpiburgerz <maxdenniel@gmail.com>
2022-10-03 10:33:59 +0200

    specific files indents

--- lua/maxime/set.lua
+++ lua/maxime/set.lua
@@ -5,16 +5,16 @@ vim.opt.relativenumber = true
 
 vim.opt.errorbells = false
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
 
 
 vim.opt.smartindent = true
 
 
 vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 
 vim.opt.termguicolors = true
