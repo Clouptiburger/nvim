e9957bbb20fcc035879be92604fe7a7ae7a60d80
Clouptiburger <63961152+Clouptiburger@users.noreply.github.com>
2022-10-12 13:51:23 +0200

    fzf

--- after/plugin/setup.lua
+++ after/plugin/setup.lua
@@ -16,8 +16,8 @@ require('telescope').setup {
 
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-pcall(require('telescope').load_extension, 'fzf')
---require('telescope').load_extension('fzf')
+-- pcall(require('telescope').load_extension, 'fzf')
+require('telescope').load_extension('fzf')
 
 
 require("fidget").setup {
