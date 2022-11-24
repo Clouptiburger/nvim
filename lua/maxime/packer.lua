6f230b25603a9e77e9d32ac944efeded52498326
Clouptiburger <maxdenniel@gmail.com>
2022-11-24 21:34:20 +0100

    removed light bulb

--- lua/maxime/packer.lua
+++ lua/maxime/packer.lua
@@ -140,6 +140,7 @@ return require('packer').startup(function(use)
             local saga = require("lspsaga")
 
             saga.init_lsp_saga({
+                code_action_icon = "",
                 symbol_in_winbar = {
                     in_custom = true
                 }
