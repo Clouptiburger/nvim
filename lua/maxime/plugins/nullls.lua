b5808826f0c1b1f471874dc906cac44e82f78db2
Maxime Denniel <maxime.denniel@siemens.com>
2023-02-16 11:59:57 +0100

    yaml fmt

--- lua/maxime/plugins/nullls.lua
+++ lua/maxime/plugins/nullls.lua
@@ -8,6 +8,7 @@ function M.config()
             -- require("null-ls").builtins.formatting.autopep8,
             require("null-ls").builtins.formatting.prettierd,
             require("null-ls").builtins.formatting.black,
+            require("null-ls").builtins.formatting.yamlfmt,
             -- require("typescript.extensions.null-ls.code-actions"),
             --         require("null-ls").builtins.diagnostics.eslint,
             --         require("null-ls").builtins.completion.spell,
