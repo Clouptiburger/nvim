d8932f31816a12d60721e161f26c712fc2790b92
Clouptiburger <maxdenniel@gmail.com>
2023-03-25 16:46:17 +0100

    added mssing file

---
+++ lua/maxime/plugins/gruvbox.lua
@@ -0,0 +1,37 @@
+local M = {
+    enabled = true,
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox"
+}
+
+
+function M.config()
+    -- setup must be called before loading the colorscheme
+    -- Default options:
+    require("gruvbox").setup({
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+            strings = false,
+            comments = false,
+            operators = false,
+            folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "",  -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {
+        },
+        dim_inactive = false,
+        transparent_mode = true,
+    })
+    vim.cmd("colorscheme gruvbox")
+end
+
+return M
