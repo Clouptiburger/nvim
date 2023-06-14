80c2313affb2746881fb7fcd3763126569e69556
Denniel, Maxime <maxime.denniel@siemens.com>
2023-06-14 16:49:09 +0200

    black formater for pthon

--- lua/maxime/plugins/nullls.lua
+++ lua/maxime/plugins/nullls.lua
@@ -7,8 +7,8 @@ function M.config()
         sources = {
             -- require("null-ls").builtins.formatting.autopep8,
             require("null-ls").builtins.formatting.prettierd,
-            -- require("null-ls").builtins.formatting.black,
-            require("null-ls").builtins.formatting.ruff,
+            require("null-ls").builtins.formatting.black,
+            -- require("null-ls").builtins.formatting.ruff,
             require("null-ls").builtins.formatting.yamlfmt,
             -- require("typescript.extensions.null-ls.code-actions"),
             --         require("null-ls").builtins.diagnostics.eslint,
