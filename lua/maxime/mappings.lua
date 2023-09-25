1632d553032980dcdf32cf3508a07e0f07f182a5
Clouptiburger <maxdenniel@gmail.com>
2023-09-25 21:34:53 +0200

    new azery mapping

--- lua/maxime/mappings.lua
+++ lua/maxime/mappings.lua
@@ -77,3 +77,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+-- remaps to use unsused azery keys
+vim.keymap.set("n", "ç", "[", { remap = true })
+vim.keymap.set("n", "à", "]", { remap = true })
