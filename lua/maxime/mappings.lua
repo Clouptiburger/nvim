833f349d5bd879b8d1ae29b2275b36338f37a699
Denniel, Maxime (DI SW STS SDPRM SYS PTF ANLST&CLD DEPL) <maxime.denniel@siemens.com>
2023-09-26 15:11:31 +0200

    added convienient AZERTY mappings

--- lua/maxime/mappings.lua
+++ lua/maxime/mappings.lua
@@ -77,3 +77,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Remap for AZERTY convienient mappings
+vim.keymap.set('n', 'à', ']', { remap = true })
+vim.keymap.set('n', 'ç', '[', { remap = true })
