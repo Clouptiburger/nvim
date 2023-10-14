a1c9cef39257212ea78574d75a1fd6894e7adedc
Clouptiburger <maxdenniel@gmail.com>
2023-10-14 17:09:13 +0200

    added treesitter context

--- lua/maxime/plugins/treesitter.lua
+++ lua/maxime/plugins/treesitter.lua
@@ -3,6 +3,7 @@ local M = {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        "nvim-treesitter/nvim-treesitter-context",
     },
     build = ':TSUpdate',
 }
