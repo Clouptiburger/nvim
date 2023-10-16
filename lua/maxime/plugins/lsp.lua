52787e16b0beb525d3a6dbf1f0e98d82ed9d7ffe
Clouptiburger <maxdenniel@gmail.com>
2023-10-16 18:09:37 +0200

    no auto accept of autocompletion

--- lua/maxime/plugins/lsp.lua
+++ lua/maxime/plugins/lsp.lua
@@ -154,7 +154,7 @@ function M.config()
             ['<C-Space>'] = cmp.mapping.complete {},
             ['<CR>'] = cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
+                select = false,
             },
             ['<Tab>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
