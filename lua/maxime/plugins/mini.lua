57ea579f9bcebe22d87ea93db782c9c8d6df8363
Clouptiburger <maxdenniel@gmail.com>
2024-05-16 18:13:04 +0200

    removed useless mappings

--- lua/maxime/plugins/mini.lua
+++ lua/maxime/plugins/mini.lua
@@ -114,13 +114,5 @@ function M.config()
     M.cursorword()
 end
 
-function M.init()
-    vim.keymap.set("n", "<leader>bd", function()
-        require("mini.bufremove").delete(0, false)
-    end)
-    vim.keymap.set("n", "<leader>bD", function()
-        require("mini.bufremove").delete(0, true)
-    end)
-end
 
 return M
