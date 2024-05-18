43cca2f8f9742a6256417a5157cd2698c6cb79e5
Maxime Denniel <maxdenniel@gmail.com>
2024-05-18 13:49:23 +0200

    added new colorschemes

---
+++ lua/maxime/plugins/colorschemes.lua
@@ -0,0 +1,9 @@
+local M = {
+	enabled = true,
+	"rebelot/kanagawa.nvim",
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{ 'kepano/flexoki-neovim',    name = 'flexoki' }
+}
+
+
+return M
