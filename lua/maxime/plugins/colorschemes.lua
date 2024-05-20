22c5edd838fc360a56e8c751e3e42591686707a6
Maxime Denniel <maxdenniel@gmail.com>
2024-05-20 13:32:10 +0200

    put all colorschemes in their own file

--- lua/maxime/plugins/colorschemes.lua
+++ lua/maxime/plugins/colorschemes.lua
@@ -1,15 +1,19 @@
 local M = {
     enabled = true,
     "rebelot/kanagawa.nvim",
-    { "ellisonleao/gruvbox.nvim",    priority = 1000, config = true, opts = ... },
-    { 'kepano/flexoki-neovim',       name = 'flexoki' },
+    { "ellisonleao/gruvbox.nvim",      priority = 1000,    config = true },
+    { 'kepano/flexoki-neovim',         name = 'flexoki' },
     { "eldritch-theme/eldritch.nvim" },
-    { "scottmckendry/cyberdream.nvim", lazy = false, priority = 1000, },
+    { "scottmckendry/cyberdream.nvim", lazy = false,       priority = 1000, },
     { 'projekt0n/github-nvim-theme' },
     { 'Mofiqul/vscode.nvim' },
     { 'marko-cerovac/material.nvim' },
     { 'Mofiqul/dracula.nvim' },
-    { "catppuccin/nvim", name = "catppuccin" }
+    { "catppuccin/nvim",               name = "catppuccin" },
+    { 'navarasu/onedark.nvim',         priority = 1000, },
+    { "EdenEast/nightfox.nvim", },
+    { 'rose-pine/neovim',              name = 'rose-pine', },
+    { "folke/tokyonight.nvim", },
 }
 
 
