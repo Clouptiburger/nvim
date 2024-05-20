c8787756f85e8da9891d1a8fc6607e164deeeac9
Maxime Denniel <maxdenniel@gmail.com>
2024-05-20 13:22:41 +0200

    reverted to default settings catpuccin

--- lua/maxime/plugins/colorschemes.lua
+++ lua/maxime/plugins/colorschemes.lua
@@ -4,15 +4,12 @@ local M = {
     { "ellisonleao/gruvbox.nvim",    priority = 1000, config = true, opts = ... },
     { 'kepano/flexoki-neovim',       name = 'flexoki' },
     { "eldritch-theme/eldritch.nvim" },
-    {
-        "scottmckendry/cyberdream.nvim",
-        lazy = false,
-        priority = 1000,
-    },
+    { "scottmckendry/cyberdream.nvim", lazy = false, priority = 1000, },
     { 'projekt0n/github-nvim-theme' },
     { 'Mofiqul/vscode.nvim' },
     { 'marko-cerovac/material.nvim' },
     { 'Mofiqul/dracula.nvim' },
+    { "catppuccin/nvim", name = "catppuccin" }
 }
 
 
